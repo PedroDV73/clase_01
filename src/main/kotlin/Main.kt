374eeae03cca76614bc7af24b38782167c17e00e
unknown <pdelvalle73>
2022-05-21 15:11:05 -0500

    [feature] - functions && operators

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import kotlin.math.*
+
 fun main() {
     // Boolean size 1bit false/true
     // Char size 16bit 'a' to 'z' '/n' '/101'
@@ -12,14 +14,79 @@ fun main() {
     */
     // String "Hello"
     // Array of int [1, 2, 3]
+    // Uso de variables
     val dayOfBirth : Int = 20
     val monthOfBirth : Int = 1
     println("My birthday is $dayOfBirth/$monthOfBirth")
+    // Concatenacion de variables
     println("My birthday is: " + dayOfBirth + "/" + monthOfBirth)
     var name : String = "Pedro"
     var lastName : String = "Del valle"
     println("My name is ${name.uppercase()} ${lastName.lowercase()}")
     name = "Peter"
     println("My name is $name $lastName")
+    // Operaciones matematicas
+    // Suma
+    println(2+2)
+    // Resta
+    println(2-2)
+    // Division
+    println(2/2)
+    // Modulos
+    println(2%2)
+    // Multiplicacion
+    println(2*2)
+    // Potencia
+    println(2.0.pow(2))
+    // Operacion Matematica
+    print(1 + (9 /3) * 2)
 
+    // Comparacion e igualdad operadores
+    // Igualdad
+    println(2 == 2)
+    // Desigualdad /  Diferencia
+    println(2 != 2)
+    // Mayor que
+    println(2 > 2)
+    // Menor que
+    println(2 < 2)
+    //  Mayor o igual que
+    println(2 >= 2)
+    // Menor o igual que
+    println(2 <= 2)
+    var anyNumber: Int = 0
+    // Operadores de asignacion
+    anyNumber += 10
+    anyNumber -= 2
+    anyNumber *= 2
+    anyNumber /= 2
+    anyNumber %= 2
+    // Incrementador y decrementador
+    anyNumber++
+    anyNumber--
+    println(anyNumber)
+    var isTrue : Boolean = true
+    var isFalse : Boolean = false
+    // Operaciones Booleanos logicos
+    // AND
+    println(isTrue && isFalse)
+    // OR
+    println(isTrue || isFalse)
+    // NOT
+    println(!isTrue)
+    helloWorld()
+    var myNumber : Int = dobleTheValue(2)
+    println(myNumber)
+    favoriteFoodPrompt("Pamela", "Sushi")
+
+}
+fun helloWorld(){
+    println("Hello World")
+}
+fun dobleTheValue(numberValue: Int) : Int{
+    return numberValue * 2
+}
+
+fun favoriteFoodPrompt(name: String, favoriteFood : String) : Unit {
+    println("$name's favorite food is $favoriteFood")
 }
